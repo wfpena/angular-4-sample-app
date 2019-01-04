26a9b2c3e4b7887c61a34b22c0d718eff4b6d13c
Wilson Pena <w.fernando76@hotmail.com>
2019-01-04 15:00:26 -0200

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-var str1 = "Hello";
-fs.readdir(path.join('./'), (err, files) => {
-  if (err) str1 += "<br>Erro: " + err + "</br>";
-  try {
-    files.forEach(file => {
-      str1 += file + "</br>");
-      console.log(file);
-    });
-  } catch(e) {
-    str1 += "</br>Error2: " + e + "</br>";
-  }
+var str1 = "Hello" + path.join('./');
+// fs.readdir(path.join('./'), (err, files) => {
+//   if (err) str1 += "<br>Erro: " + err + "</br>";
+//   try {
+//     files.forEach(file => {
+//       str1 += file + "</br>");
+//       console.log(file);
+//     });
+//   } catch(e) {
+//     str1 += "</br>Error2: " + e + "</br>";
+//   }
 
-})
+// })
 
 const app = express();
 
